1383ef0ab83edb775e44e6c10221cb45dd8d8ce2
ericroco <erickrodas559@gmail.com>
2024-06-13 16:54:08 -0500

    clase02

--- EjemploHerencia02/HerenciaUno/src/paquete6/Ejecutor2.java
+++ EjemploHerencia02/HerenciaUno/src/paquete6/Ejecutor2.java
@@ -12,35 +12,45 @@ import java.util.Scanner;
  * @author reroes
  */
 public class Ejecutor2 {
+
     public static void main(String[] args) {
         // Ingresar estudiante de tipo presencial por teclado.
         // El usuario decide cuando terminar
         // No utilizar super.toString en EstudiantePresencial
         // Debe existir 1 sola impresión de todos los estudiantes presenciales
         // No usar arreglos
-        
-        // Trabajo clases 16 junio 2022
+
+        // Trabajo clases 13 junio 2024
         Scanner entrada = new Scanner(System.in);
         boolean bandera = true;
         String cadena = "";
-        
-        while(bandera){
-            System.out.println("ingresa un saludo");
-            String saludo = entrada.nextLine();
-            cadena = String.format("%s%s\n", cadena, saludo);
+
+        while (bandera) {
+            System.out.println("Ingrese el nombre");
+            String nombre = entrada.nextLine();
+            System.out.println("Ingrese el apellido");
+            String apellido = entrada.nextLine();
+            System.out.println("Ingrese la id");
+            String id = entrada.nextLine();
+            System.out.println("Ingrese la edad");
+            int edad = entrada.nextInt();
+            System.out.println("Cual es el costo de cada credito");
+            double costoCredito = entrada.nextDouble();
+            System.out.println("Cual es la cantidad de creditos");
+            int numCreditos = entrada.nextInt();
+
+            EstudiantePresencial presencial = new EstudiantePresencial(nombre,
+                    apellido, id, edad, costoCredito, numCreditos);
             System.out.println("Desea salir, pulse S");
+            entrada.nextLine();
+            cadena = String.format("%s%s\n", cadena, presencial);
             String opcion = entrada.nextLine();
-            if(opcion.equals("S")){
+            if (opcion.toUpperCase().equals("S")) {
                 bandera = false;
             }
         }
-        
+
         System.out.printf("%s\n", cadena);
-        
-        
+
     }
 }
-
-
-
-
--- EjemploHerencia02/HerenciaUno/src/paquete6/EstudiantePresencial.java
+++ EjemploHerencia02/HerenciaUno/src/paquete6/EstudiantePresencial.java
@@ -1,52 +1,64 @@
-
 package paquete6;
 
-public class EstudiantePresencial extends Estudiante{
-    
+public class EstudiantePresencial extends Estudiante {
+
     int numeroCreditos;
     double costoCredito;
     double matriculaPresencial;
-    
-    public EstudiantePresencial(String n, String a, String iden, int ed){
+
+    public EstudiantePresencial(String n, String a, String iden, int ed) {
         super(n, a, iden, ed);
     }
-    
-    public EstudiantePresencial(String n, String a, String iden, int ed, 
-            double costo, int creditos){
+
+    public EstudiantePresencial(String n, String a, String iden, int ed,
+            double costo, int creditos) {
         super(n, a, iden, ed);
         costoCredito = costo;
         numeroCreditos = creditos;
     }
+
     //  Métodos establecer y calcular para los datos o atributos de la clase
     // 2.  Método establecerNumeroCreditos(numero: Real)
-    public void establecerNumeroCreditos(int numero){
+    public void establecerNumeroCreditos(int numero) {
         numeroCreditos = numero;
     }
-    
+
     // 3.  Método establecerCostoCredito(valor: Real)
-    public void establecerCostoCredito(double valor){
+    public void establecerCostoCredito(double valor) {
         costoCredito = valor;
     }
 
     // 4.  Método calcularMatriculaPresencial()
-    public void calcularMatriculaPresencial(){
+    public void calcularMatriculaPresencial() {
         matriculaPresencial = numeroCreditos * costoCredito;
     }
 
     //  Métodos obtener para los datos o atributos de la clase
     // 5. Método obtenerNumeroCreditos() : Entero
-    public int obtenerNumeroCreditos(){
-        return numeroCreditos; 
+    public int obtenerNumeroCreditos() {
+        return numeroCreditos;
     }
 
     // 6. Método obtenerCostoCredito() : Real
-    public double obtenerCostoCredito(){
+    public double obtenerCostoCredito() {
         return costoCredito;
     }
 
     // 7. Método obtenerMatriculaPresencial() : Real
-    public double obtenerMatriculaPresencial(){
+    public double obtenerMatriculaPresencial() {
         return matriculaPresencial;
     }
-    
+
+    @Override
+    public String toString() {
+        String cadena = String.format("Nombre del estudiante: %s\n"
+                + "Apellido del estudiante: %s\n"
+                + "Id: %s\nEdad: %s\nCosto creditos: %.2f\n"
+                + "Numero creditos %d\n", obtenerNombresEstudiante(),
+                 obtenerApellidoEstudiante(), obtenerIdentificacionEstudiante(),
+                 obtenerEdadEstudiante(), obtenerCostoCredito(),
+                 obtenerNumeroCreditos());
+        return cadena;
+    }
+
 }
